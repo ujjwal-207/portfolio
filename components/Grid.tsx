e0ab02504eb7444a4f125445732e1cd0e0a94840
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-07-03 18:21:19 +0545

    bento grid

---
+++ components/Grid.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data/Index'
+
+const Grid = () => {
+  return (
+    <section id='about'>
+        <BentoGrid>
+            {gridItems.map((item,i)=>(
+                <BentoGridItem
+                id={item.id}
+                key={item.id}
+                title={item.title}
+                description={item.description}
+
+                 />
+            ))}
+        </BentoGrid>
+    </section>
+  )
+}
+
+export default Grid
