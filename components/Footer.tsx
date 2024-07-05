0cbaa6dd9b915620910b45f910dfb4df1bcc4b1d
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-07-05 21:07:29 +0545

    text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,12 +17,11 @@ const Footer = () => {
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Ready to take <span className="text-purple">your</span> digital
-          presence to the next level?
+          Ready to take <span className="text-purple">your</span> ideas
+          to the next level?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you
-          achieve your goals.
+          Reach out to me today and let&apos;s discuss for collaboration.
         </p>
         <a href="ujjwalnepal715@gmail.com">
           <MagicButton
